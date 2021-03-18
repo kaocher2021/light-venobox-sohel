704079b2c552870d57c842b8288f7e9e8ea05137
Your Name <kaocheraub@gmail.com>
2021-03-19 05:32:37 +0600

    boom

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+
+
+$(function(){
+
+    $('.gal-head').click(function(){
+        $('h1').fadeOut(2000);
+
+    });
+
+
+    $('.gal-head').click(function(){
+        $('h1').fadeIn(2000);
+
+    });
+
+
+    $('.venobox').venobox(); 
+
+
+});
